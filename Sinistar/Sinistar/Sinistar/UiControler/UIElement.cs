a9dd14dcbf6966f548a65ed3df3cf95a4203fa67
130113 <130113@S020RMD114-06>
2019-01-14 12:06:20 -0600

    Added the UI Controller and UI element, nothing is really useful yet.

---
+++ Sinistar/Sinistar/Sinistar/UiControler/UIElement.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Sinistar.UiControler
+{
+    abstract class UIElement
+    {
+        public int offsetX;
+        public int offsetY;
+        public double scaleX;
+        public double scaleY;
+
+        public int sizeX;
+        public int sizeY;
+        public double anchorPointX;
+        public double anchorPointY;
+
+        public UIElement(int sx, int sy, int ox, int oy)
+        {
+            offsetX = ox;
+            offsetY = oy;
+            scaleX = sx;
+            scaleY = sy;
+        }
+
+        public abstract void draw(SpriteBatch spriteBatch, int x, int y);
+
+    }
+
+    public class UIText
+    {
+
+    }
+
+    public class UIImage
+    {
+
+    }
+
+    public class UIButton
+    {
+
+    }
+
+}
---
+++ Sinistar/Sinistar/Sinistar/UiControler/UiController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Sinistar.UiControler
+{
+    class UiController
+    {
+
+        private SpriteBatch spriteBatch;
+        private GraphicsDevice graphicsDevice;
+
+        private int viewXSize;
+        private int viewYSize;
+        private List<UIElement> elements;
+
+        public UiController(SpriteBatch spriteBatch, GraphicsDevice graphicsDevice)
+        {
+            this.spriteBatch = spriteBatch;
+            this.graphicsDevice = graphicsDevice;
+            this.elements = new List<UIElement>();
+        }
+
+
+        public void updateScreenSize()
+        {
+            viewXSize = graphicsDevice.Viewport.Width;
+            viewYSize = graphicsDevice.Viewport.Height;
+        }
+
+
+        public void addElement(UIElement element)
+        {
+            elements.Add(element);
+        }
+        public void addElement(UIElement element, int ox, int oy, int sx, int sy)
+        {
+            element.offsetX = ox;
+            element.offsetY = oy;
+            element.scaleX = sx;
+            element.scaleY = sy;
+
+            elements.Add(element);
+        }
+
+        public void drawElement(UIElement element)
+        {
+            //Takes the view size multiplies it by the scale position, adds the offset, then uses the anchor point to position it onto the screen
+            int x = (int)(viewXSize * element.scaleX + element.offsetX + element.sizeX * element.anchorPointX);
+            int y = (int)(viewYSize * element.scaleY + element.offsetY + element.sizeY * element.anchorPointY);
+
+            element.draw(spriteBatch, x, y);
+        }
+
+        
+        public void draw()
+        {
+            for (int i = 0; i <= elements.Count; i++) {
+                elements.ElementAt(i);
+            }
+
+        }
+
+
+        /// <summary>
+        ///     This draws the provided text using the sprite font, 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        ///
+        public void drawText(string text, SpriteFont font, int textSize, double x, double y)
+        {
+            spriteBatch.DrawString()
+        }
+
+
+        
+    }
+}
