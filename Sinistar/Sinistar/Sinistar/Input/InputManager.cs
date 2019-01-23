8a5a22ab43e2ae34457b203f92c6f065b615da09
hunte <hunte@HUNTER-LAPTOP>
2019-01-23 10:51:18 -0600

    Fixed get direction method

---
+++ Sinistar/Sinistar/Sinistar/Input/InputManager.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Sinistar.Input;
+using Sinistar.Input.Listeners;
+using Sinistar.Input.Binders;
+
+namespace Sinistar.Input
+{
+    class InputManager
+    {
+        
+        Dictionary<Keys, bool> keyInputs;
+        List<GeneralInputListener> generalListeners;
+        List<GamepadListener> gamepadListeners;
+        List<KeyboardListener> keyboardListeners;
+        List<MouseListener> mouseListeners;
+
+        Dictionary<Keys, KeyboardBind> keyboardBinds;
+        Dictionary<Keys, GamepadBind> gamepadBinds;
+        Dictionary<Keys, MouseBind> mouseBinds;
+
+        GamePadState oldGPDState1;
+        GamePadState oldGPDState2;
+        GamePadState oldGPDState3;
+        GamePadState oldGPDState4;
+
+        KeyboardState oldKeyState;
+        MouseState oldMosState;
+
+
+        public InputManager()
+        {
+            generalListeners = new List<GeneralInputListener>();
+            gamepadListeners = new List<GamepadListener>();
+            keyboardListeners = new List<KeyboardListener>();
+            mouseListeners = new List<MouseListener>();
+
+            keyboardBinds = new Dictionary<Keys, KeyboardBind>();
+            gamepadBinds = new Dictionary<Keys, GamepadBind>();
+            mouseBinds = new Dictionary<Keys, MouseBind>();
+        }
+
+        /// <summary>
+        ///     Registers a listener to this manager
+        /// </summary>
+        /// <param name="listener">The listener class</param>
+        public void addListener(GeneralInputListener listener) 
+        {
+            generalListeners.Add(listener);
+        }
+        /// <summary>
+        ///     Removes a listener to this manager
+        /// </summary>
+        /// <param name="listener">The listener class</param>
+        public void removeListener(GeneralInputListener listener)
+        {
+            generalListeners.Remove(listener);
+        }
+
+        /// <summary>
+        ///     Adds a Gamepad input handler only to this manager
+        /// </summary>
+        /// <param name="listener">The gamepad listener class</param>
+        public void addGamepadListener(GamepadListener listener)
+        {
+            gamepadListeners.Add(listener);
+        }
+        /// <summary>
+        ///     Removes a Gamepad input handler only to this manager
+        /// </summary>
+        /// <param name="listener">The gamepad listener class</param>
+        public void removeGamepadListener(GamepadListener listener)
+        {
+            gamepadListeners.Remove(listener);
+        }
+
+        /// <summary>
+        ///     Adds a Keyboard input handler only to this manager
+        /// </summary>
+        /// <param name="listener">The keyboard listener class</param>
+        public void addKeyboardListener(KeyboardListener listener)
+        {
+            keyboardListeners.Add(listener);
+        }
+        /// <summary>
+        ///     Removes a Keyboard input handler only to this manager
+        /// </summary>
+        /// <param name="listener">The keyboard listener class</param>
+        public void removeKeyboardListener(KeyboardListener listener)
+        {
+            keyboardListeners.Remove(listener);
+        }
+
+        /// <summary>
+        ///     Adds a Mouse input handler only to this manager
+        /// </summary>
+        /// <param name="listener">The mouse listener class</param>
+        public void addMouseListener(MouseListener listener)
+        {
+            mouseListeners.Add(listener);
+        }
+        /// <summary>
+        ///     Removes a Mouse input handler only to this manager
+        /// </summary>
+        /// <param name="listener">The mouse listener class</param>
+        public void removeMouseListener(MouseListener listener)
+        {
+            mouseListeners.Remove(listener);
+        }
+
+        /// <summary>
+        ///     Sets a bind to a specific keyboard key. When this key is pressed the method will be called
+        /// </summary>
+        /// <param name="bind">The binded class</param>
+        /// <param name="key">The key</param>
+        public void setKeyboardBind(KeyboardBind bind, Keys key)
+        {
+            keyboardBinds.Add(key, bind);
+        }
+
+        /// <summary>
+        ///     Unsets a bind to a specific keyboard key. When this key is pressed the method will be called
+        /// </summary>
+        /// <param name="bind">The binded class</param>
+        /// <param name="key">The key</param>
+        public void removeKeyboardBind(Keys key)
+        {
+            keyboardBinds.Remove(key);
+        }
+
+
+
+        private void fireInputBegan(InputDeviceType inputType, GamePadState gpdState, KeyboardState keyState, MouseState mosState, Keys keyBtn, GamepadCode gpdBtn, MouseCode mosBtn)
+        {
+            InputObject input = new InputObject(inputType, gpdState, mosState, keyBtn, gpdBtn, mosBtn);
+
+            //General
+            for (int i = 0; i < generalListeners.Count; i++)
+            {
+                GeneralInputListener listener = generalListeners[i];
+                listener.InputBegan(input);
+                switch (inputType)
+                {
+                    case (InputDeviceType.Gamepad):
+                        listener.GamepadInputBegan(input);
+                        break;
+                    case (InputDeviceType.Keyboard):
+                        listener.KeyboardInputBegan(input);
+                        break;
+                    case (InputDeviceType.Mouse):
+                        listener.MouseInputBegan(input);
+                        break;
+                }
+            }
+            //Others
+            switch (inputType)
+            {
+                case (InputDeviceType.Gamepad):
+                    for (int i = 0; i < gamepadListeners.Count; i++)
+                    {
+                        GamepadListener listener = gamepadListeners[i];
+                        listener.InputBegan(input);
+                    }
+                    break;
+
+                case (InputDeviceType.Keyboard):
+                    for (int i = 0; i < keyboardListeners.Count; i++)
+                    {
+                        KeyboardListener listener = keyboardListeners[i];
+                        listener.InputBegan(input);
+                    }
+                    if (keyboardBinds.ContainsKey(keyBtn))
+                    {
+                        keyboardBinds[keyBtn].Began();
+                    }
+                    break;
+
+                case (InputDeviceType.Mouse):
+                    for (int i = 0; i < mouseListeners.Count; i++)
+                    {
+                        MouseListener listener = mouseListeners[i];
+                        listener.InputBegan(input);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        private void fireInputEnd(InputDeviceType inputType, GamePadState gpdState, KeyboardState keyState, MouseState mosState, Keys keyBtn, GamepadCode gpdBtn, MouseCode mosBtn)
+        {
+            InputObject input = new InputObject(inputType, gpdState, mosState, keyBtn, gpdBtn, mosBtn);
+            //General
+            for (int i = 0; i < generalListeners.Count; i++)
+            {
+                GeneralInputListener listener = generalListeners[i];
+                listener.InputEnded(input);
+                switch (inputType)
+                {
+                    case (InputDeviceType.Gamepad):
+                        listener.GamepadInputEnded(input);
+                        break;
+                    case (InputDeviceType.Keyboard):
+                        listener.KeyboardInputEnded(input);
+                        break;
+                    case (InputDeviceType.Mouse):
+                        listener.MouseInputEnded(input);
+                        break;
+                }
+            }
+            //Others
+            switch (inputType)
+            {
+                case (InputDeviceType.Gamepad):
+                    for (int i = 0; i < gamepadListeners.Count; i++)
+                    {
+                        GamepadListener listener = gamepadListeners[i];
+                        listener.InputEnded(input);
+                    }
+                    break;
+
+                case (InputDeviceType.Keyboard):
+                    for (int i = 0; i < keyboardListeners.Count; i++)
+                    {
+                        KeyboardListener listener = keyboardListeners[i];
+                        listener.InputEnded(input);
+                    }
+                    if (keyboardBinds.ContainsKey(keyBtn))
+                    {
+                        keyboardBinds[keyBtn].Ended();
+                    }
+                    break;
+
+                case (InputDeviceType.Mouse):
+                    for (int i = 0; i < mouseListeners.Count; i++)
+                    {
+                        MouseListener listener = mouseListeners[i];
+                        listener.InputEnded(input);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        /// <summary>
+        ///     Updates the input
+        /// </summary>
+        public void updateInput()
+        {
+            GamePadState newGPDState1 = GamePad.GetState(PlayerIndex.One); //I guess we are only using one...
+            //...
+            KeyboardState newKeyState = Keyboard.GetState();
+            MouseState newMosState = Mouse.GetState();
+
+            //Keyboard
+            if (newKeyState != oldKeyState)
+            {
+                Keys[] oldKeys = oldKeyState.GetPressedKeys();
+                Keys[] newKeys = newKeyState.GetPressedKeys();
+                Dictionary<Keys, bool> keyDiff = new Dictionary<Keys, bool>(); //Key : IsPressed
+
+                List<Keys> pressed = new List<Keys>();
+                List<Keys> released = new List<Keys>();
+
+                int numPressed = 0;
+                int numReleased = 0;
+                for (int i=0; i<newKeys.Length; i++)
+                {
+                    numPressed++;
+                    keyDiff.Add(newKeys[i], true);
+                }
+                for (int i = 0; i < oldKeys.Length; i++)
+                {
+                    Keys oldKey = oldKeys[i];
+                    if (keyDiff.ContainsKey(oldKey)) //Could use contains method for a list? But performace?
+                    {
+                        numPressed--;
+                        keyDiff.Remove(oldKey); 
+                    }
+                    else
+                    {
+                        numReleased++;
+                        fireInputEnd(InputDeviceType.Keyboard, newGPDState1, newKeyState, newMosState, oldKey, GamepadCode.None, MouseCode.None);
+                    }
+                }
+                for (int i = 0; i < keyDiff.Keys.Count; i++)
+                {
+                    Keys key = keyDiff.Keys.ElementAt(i);
+                    bool isPressed = keyDiff[key];
+                    if (isPressed)
+                        fireInputBegan(InputDeviceType.Keyboard, newGPDState1, newKeyState, newMosState, key, GamepadCode.None, MouseCode.None);
+                }
+            }
+
+            //Gamepad
+            if (oldGPDState1 != newGPDState1)
+            {
+
+            }
+
+            oldGPDState1 = newGPDState1;
+            oldKeyState = newKeyState;
+            oldMosState = newMosState;
+        }
+
+    }
+    
+}
---
+++ Sinistar/Sinistar/Sinistar/Input/InputObject.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Sinistar.Input
+{
+
+    public enum InputDeviceType
+    {
+        Keyboard,
+        Mouse,
+        Gamepad,
+        Joystick,
+        DPad
+    }
+
+    public enum GamepadCode
+    {
+        None,
+        GamepadX, GamepadA, GamepadB, GamepadY,
+        GamepadRB, GamepadLB, GamepadRT, GamepadLT,
+        GamepadStart, GamepadBack, GamepadBigButton,
+        DPadL, DPadR, DPadU, DPadD,
+        JoysticLeft, JoystickRight
+    }
+    public enum MouseCode
+    {
+        None,
+        MouseLeftBtn, MouseRightBtn, MouseMiddleBtn,
+        MouseScrollUp, MouseScrollDown
+    }
+
+    public class InputObject
+    {
+        public static Keys KeyboardMoveUp = Keys.W;
+        public static Keys KeyboardMoveDown = Keys.S;
+        public static Keys KeyboardMoveLeft = Keys.A;
+        public static Keys KeyboardMoveRight = Keys.D;
+
+        public static MouseCode MouseMoveUp = MouseCode.None;
+        public static MouseCode MouseMoveDown = MouseCode.None;
+        public static MouseCode MouseMoveLeft = MouseCode.None;
+        public static MouseCode MouseMoveRight = MouseCode.None;
+
+        
+        public readonly InputDeviceType deviceType;
+        public readonly GamepadCode gamepadCode;
+        public readonly Keys keyboardCode;
+        public readonly MouseCode mouseCode;
+
+        private GamePadState gpdState;
+        private MouseState mosState;
+
+        public InputObject(InputDeviceType deviceType, GamePadState gpdState, MouseState mosState, Keys keyBtn, GamepadCode gpdBtn, MouseCode musBtn)
+        {
+            this.gamepadCode = gpdBtn;
+            this.keyboardCode = keyBtn;
+            this.mouseCode = musBtn;
+
+            this.gpdState = gpdState;
+            this.mosState = mosState;
+        }
+
+        /// <summary>
+        ///     Attempts to find the position of the input given that the input can produce a position.
+        /// </summary>
+        /// <returns></returns>
+        public Point getPosition()
+        {
+            if (deviceType == InputDeviceType.Mouse)
+            {
+                return new Point(mosState.X, mosState.Y);
+            }
+            return Point.Zero;
+        }
+
+        /// <summary>
+        ///     Attempts to find the direction of the input given that the input can produce a direction.
+        /// </summary>
+        /// <returns></returns>
+        public Vector2 getDirection()
+        {
+            Vector2 direction = new Vector2(0, 0);
+
+            //Joysticks
+            if (deviceType == InputDeviceType.Joystick)
+            {
+                if (gamepadCode == GamepadCode.JoysticLeft)
+                {
+                    Vector2 mv = gpdState.ThumbSticks.Left;
+                    mv.Normalize();
+                    return mv;
+                }
+                else
+                {
+                    Vector2 mv = gpdState.ThumbSticks.Right;
+                    mv.Normalize();
+                    return mv;
+                }
+            }
+            //Dpad
+            else if (deviceType == InputDeviceType.DPad)
+            {
+                switch (gamepadCode)
+                {
+                    case (GamepadCode.DPadU):
+                        direction.Y += 1;
+                        break;
+                    case (GamepadCode.DPadD):
+                        direction.Y -= 1;
+                        break;
+                    case (GamepadCode.DPadL):
+                        direction.X -= 1;
+                        break;
+                    case (GamepadCode.DPadR):
+                        direction.X += 1;
+                        break;
+                }
+            }
+            //Keyboard
+            else if (deviceType == InputDeviceType.Keyboard)
+            {
+                switch (keyboardCode)
+                {
+                    case (Keys.W):
+                        direction.Y += 1;
+                        break;
+                    case (Keys.S):
+                        direction.Y -= 1;
+                        break;
+                    case (Keys.A):
+                        direction.X -= 1;
+                        break;
+                    case (Keys.D):
+                        direction.X += 1;
+                        break;
+                }
+                
+            }
+
+            direction.Normalize();
+            return direction;
+        }
+    }
+
+}
